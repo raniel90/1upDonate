8f7dfc84b0ea7917115456888005ed084f8bf035
raniel90 <raniel90@gmail.com>
2015-10-28 15:08:42 -0200

    Ajustes na cor dos dashboards

--- src/br/com/upe/bean/DashBoardBean.java
+++ src/br/com/upe/bean/DashBoardBean.java
@@ -1,6 +1,8 @@
 package br.com.upe.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -10,6 +12,7 @@ import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.CategoryAxis;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.LineChartSeries;
+import org.primefaces.model.chart.MeterGaugeChartModel;
  
 @ManagedBean
 public class DashBoardBean implements Serializable {
@@ -19,17 +22,23 @@ public class DashBoardBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private LineChartModel areaModelDoacoesPorSemana;
+	private MeterGaugeChartModel meterGaugeDoacoesDia;
  
     @PostConstruct
     public void init() {
     	createAreaModelDoacoesPorSemana();
+    	createMeterGaugeDoacoesDoDia();
     }
  
     public LineChartModel getAreaModelDoacoesPorSemana() {
         return areaModelDoacoesPorSemana;
     }
      
-    private void createAreaModelDoacoesPorSemana() {
+    public MeterGaugeChartModel getMeterGaugeDoacoesDia() {
+		return meterGaugeDoacoesDia;
+	}
+
+	private void createAreaModelDoacoesPorSemana() {
     	areaModelDoacoesPorSemana = new LineChartModel();
  
         LineChartSeries doacoes = new LineChartSeries();
@@ -46,6 +55,7 @@ public class DashBoardBean implements Serializable {
         areaModelDoacoesPorSemana.setLegendPosition("ne");
         areaModelDoacoesPorSemana.setStacked(true);
         areaModelDoacoesPorSemana.setShowPointLabels(true);
+        areaModelDoacoesPorSemana.setSeriesColors("cc0000, cc0000, cc0000, cc0000");
          
         Axis xAxis = new CategoryAxis("Semanas");
         areaModelDoacoesPorSemana.getAxes().put(AxisType.X, xAxis);
@@ -53,6 +63,29 @@ public class DashBoardBean implements Serializable {
         yAxis.setLabel("Doadores");
         yAxis.setMin(0);
         yAxis.setMax(100);
+    }
+    
+    private MeterGaugeChartModel initMeterGaugeModel() {
+        @SuppressWarnings("serial")
+		List<Number> intervals = new ArrayList<Number>(){{
+            add(10);
+            add(20);
+            add(50);
+            add(100);
+        }};
+         
+        return new MeterGaugeChartModel(30, intervals);
+    }
+    
+    private void createMeterGaugeDoacoesDoDia() {
+         
+        meterGaugeDoacoesDia = initMeterGaugeModel();
+        meterGaugeDoacoesDia.setTitle("Doações por dia");
+        meterGaugeDoacoesDia.setSeriesColors("66cc66,93b75f,E7E658,cc6666");
+        meterGaugeDoacoesDia.setGaugeLabel("Doações");
+        meterGaugeDoacoesDia.setGaugeLabelPosition("bottom");
+        meterGaugeDoacoesDia.setLabelHeightAdjust(110);
+        meterGaugeDoacoesDia.setIntervalOuterRadius(100);
     }
      
 }
